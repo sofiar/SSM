768b2ff888cd9283fbfde827dd7a3058171efb06
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2017-05-31 14:34:50 -0300

    Viterbi comentado

--- funAux.R
+++ funAux.R
@@ -1,5 +1,14 @@
 ################ Funciones auxiliares ##############################
 
+
+## Algoritmo recursivo de viterbi para calcular la secuencia de estados mas probable
+
+# x = vector valores de las tres variables para cada tiempo del individuo en cuestion
+# m = cantidad total de posibles estados (en este caso son cuatro)
+# gamma = matriz con valores de las medias (estimados)
+# allprobs = matriz con probabilidad de cada estado en cada tiempo
+# delta = probas iniciales para cada estado
+
 HMM.viterbi <- function(x, m, gamma, allprobs, delta=NULL, ...)
 {
   if(is.null(delta)) delta <- solve(t(diag(m) - gamma + 1), rep(1, m))
