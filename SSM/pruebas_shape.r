fb7eca823696ac3018f850373445de77d5f01f08
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2017-07-26 16:43:08 -0300

    corregi el CRW. cambie lo del di, estaba mal el codigo

--- SSM/pruebas_shape.r
+++ SSM/pruebas_shape.r
@@ -2,11 +2,12 @@
 
 #### verdadera
 
+
 library(circular) 
 library(mvtnorm)
 source('/home/sofia/proyecto_doctoral/pruebas/SSM/funaux.R')
 
-nsteps <- 500 # number of moves performed by the animal
+nsteps <- 800 # number of moves performed by the animal
 
 # Parametrs of the model:
 #t_scale = 2
@@ -17,7 +18,7 @@ t_k =20
 
 # simulate the movement
 tu<-numeric(nsteps)
-tu[1]= rvonmises(1, mu=t_mu, kappa=t_k) # first angle
+tu[1]= rvonmises(1, mu=circular(t_mu), kappa=t_k) # first angle
 tm<-rgamma(nsteps,shape=t_shape,scale=t_mean/t_shape)
 ls <- numeric(nsteps) + 1 # assume constant speed for now
 
@@ -31,20 +32,18 @@ di <- numeric(nsteps)
 di[1] <- runif(1) * 2*pi # initial movement direction
 
 for(i in 2:nsteps){
-  tu[i] <- rvonmises(1, mu=tu[i-1], kappa=t_k) # turning angle
+  tu[i] <- rvonmises(1, mu=circular(di[i-1]), kappa=t_k) # turning angle
   
   t[i] <- t[i-1] + tm[i]
-  di[i] <- di[i-1]+tu[i]
+  di[i] <- tu[i]
   x[i] <- x[i-1] + cos(di[i]) * (ls[i]) * tm[i]
   y[i] <- y[i-1] + sin(di[i]) * (ls[i]) * tm[i]
 }
 
 # we observe at time dt (pay attention to this parameter)
 
-dt <- 5 # time interval for observations
+dt <- 10 # time interval for observations
 oz = observe(x,y,t,dt) # these are the observed data
-
-
 
 plot(x,y,type="p", asp=1)
 points(oz$sx,oz$sy,col=2, pch=16)
@@ -52,19 +51,12 @@ lines(oz$sx,oz$sy,col=2)
 
 ps=pathelements(oz$sx,oz$sy)
 
-bb=acf(as.circular(ps$turns))
-llmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
-plot(bb$lag[2:length(bb$lag)],bb$acf[2:length(bb$lag)])
+bb=acf(circular(ps$turns))
+llmmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
 
 
 aa=acf(ps$steps)
 llmm=lm(aa$lag[2:length(aa$lag)]~aa$acf[2:length(aa$lag)])
-
-
-
-### funcion de autocorrelacion turns)
-bb=acf(ps$turns,plot=FALSE)
-llmmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
 
 
 posta=c(mean(ps$steps),
@@ -76,18 +68,20 @@ posta=c(mean(ps$steps),
         mean(aa$acf),
         llmm$coefficients[1],
         llmm$coefficients[2],
-        cha(oz$sx,oz$sy),
-        mean(bb$acf),
+        cdt(oz$sx,oz$sy,oz$st),
+        it(ps$steps,oz$sx,oz$sy),
+        sd.circular(circular(ps$turns)),
+        angular.deviation(circular(ps$turns)),
         llmmm$coefficients[1],
-        llmmm$coefficients[2])
-
+        llmmm$coefficients[2]
+        )
 
 #################################   Simulaciones   #######################################
-nsims=1e5
-T_mean = runif(nsims,0.5,4)
+nsims=1e3
+T_mean = runif(nsims,0.5,5)
 T_shape = runif(nsims,1,10)
 T_mu = runif(nsims,-pi,pi)
-T_k = runif(nsims,1,100)
+T_k = runif(nsims,0,100)
 val1=numeric(nsims)
 val2=numeric(nsims)
 val3=numeric(nsims)
@@ -101,6 +95,8 @@ val10=numeric(nsims)
 val11=numeric(nsims)
 val12=numeric(nsims)
 val13=numeric(nsims)
+val14=numeric(nsims)
+val15=numeric(nsims)
 # simulate the movement
 for (k in 1:nsims)
 {
@@ -112,7 +108,7 @@ t_k=T_k[k]
 tm<-rgamma(nsteps,shape=t_shape,scale=t_mean/t_shape)
 ls <- numeric(nsteps) + 1 # assume constant speed for now
 tu=numeric(nsteps)
-tu[1]= rvonmises(1, mu=t_mu, kappa=t_k) # first angle
+tu[1]= rvonmises(1, mu=circular(t_mu), kappa=t_k) # first angle
 
 
 x <- numeric(nsteps)
@@ -122,9 +118,9 @@ di <- numeric(nsteps)
 di[1] <- runif(1) * 2*pi # initial movement direction
 
 for(i in 2:nsteps){
-  tu[i] <- rvonmises(1, mu=tu[i-1], kappa=t_k) # turning angle
-  t[i] <- t[i-1] + tm[i]
-  di[i] <- di[i-1]+tu[i]
+  tu[i] <- rvonmises(1, mu=circular(di[i-1]), kappa=t_k) # turning angle
+  t[i] <- t[i-1] + tm[i]  ###tiempo
+  di[i] <- tu[i]
   x[i] <- x[i-1] + cos(di[i]) * (ls[i]) * tm[i]
   y[i] <- y[i-1] + sin(di[i]) * (ls[i]) * tm[i]
 }
@@ -147,39 +143,44 @@ val7[k]=mean(aa$acf)
 val8[k]=llmm$coefficients[1]
 val9[k]=llmm$coefficients[2]
 
-val10[k]=cha(oz$sx,oz$sy)
+val10[k]=sqrt((mean(ps$cosine))^2+(mean(ps$sine))^2)
 
 ### funcion de autocorrelacion turns)
 bb=acf(ps$turns,plot=FALSE)
-llmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
-val11[k]=mean(bb$acf)
-val12[k]=llmm$coefficients[1]
-val13[k]=llmm$coefficients[2]
-
-
+llmmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
+val11[k]=it(ps$steps,oz$sx,oz$sy)
+val12[k]=sd.circular(circular(ps$turns))
+val13[k]=angular.deviation(circular(ps$turns))
+val14[k]=llmmm$coefficients[1]
+val15[k]=llmmm$coefficients[2]
 
 }
 
 ############ Comparando summaries a ojo ###################
+par(mfrow=c(1,2))
+pp=which(T_k>=1 & T_k<=40)
+valor=val10
+hist(valor[pp],freq =FALSE)
+hist(valor[-pp],freq= FALSE)
 
 ##### comparacion por distancia
 
-Summaries=cbind(val1,val2,val3,val4,val6,val7,val8,val9)
+Summaries=cbind(val1,val2,val3,val4,val6,val7,val8,val9,val12)
 A=cov(na.omit(Summaries))
-Mahal=apply(Summaries,1,mahalanobis,center=posta[c(1,2,3,4,6,7,8,9)],cov=A)
+Mahal=apply(Summaries,1,mahalanobis,center=posta[c(1,2,3,4,6,7,8,9,12)],cov=A)
 
 which=numeric(nsims)
-which[order(Mahal)[1:70]]=1
+which[order(Mahal)[1:20]]=1
 
 par(mfrow=c(1,3))
 
-plot(T_shape[which==1],T_mean[which==1],xlim=c(1,10),ylim=c(0.5,4))
+plot(T_shape[which==1],T_mean[which==1],xlim=c(1,10),ylim=c(0.5,5))
 points(2,2,col='red',pch=21,bg='red')
 
 plot(T_shape[which==1],T_k[which==1],xlim=c(1,10),ylim=c(1,100))
 points(2,20,col='red',pch=21,bg='red')
 
-plot(T_mean[which==1],T_k[which==1],xlim=c(0.5,4),ylim=c(1,100))
+plot(T_mean[which==1],T_k[which==1],xlim=c(0.5,5),ylim=c(1,100))
 points(2,20,col='red',pch=21,bg='red')
 
 
