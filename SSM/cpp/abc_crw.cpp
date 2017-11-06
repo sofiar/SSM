12354cbeb1ad56fe2612284a17bceb6efc10b4b7
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2017-11-06 09:47:20 -0300

    cambie el tema de los nobs, habia tryectorias que fallaban las simulaciones

---
+++ SSM/cpp/abc_crw.cpp
@@ -0,0 +1,156 @@
+// Si quiero llamar a otra funcon .cpp y usarla 
+#include <Rcpp.h>
+using namespace Rcpp;
+// [[Rcpp::export]]
+
+NumericMatrix cppCRW_exp(double k, double w, double ns, double maxx){
+  RNGScope scp;
+  Function rvonmises("rvonmises");
+  Function rexp("rexp");
+  Function runif("runif");
+  
+  double nk = k;
+  double nw = w;
+  double nsteps = ns;
+  double mm = maxx;
+  
+  NumericVector vtm(nsteps);
+  NumericVector vls(nsteps);
+  
+  vls = NumericVector(nsteps)+1;
+  
+  
+  NumericVector aux2(1);
+  NumericVector aux(1);
+  
+  //NumericMatrix tu(nsteps,1);
+  NumericVector tu(nsteps);
+  NumericVector x(nsteps);
+  NumericVector y(nsteps);
+  NumericVector t(nsteps);
+  NumericVector d(nsteps);
+  
+  vtm=rexp(nsteps,nw);
+  aux=rvonmises(1, 0, nk);
+  
+  //tu[0] = aux[0];// first angle
+  
+  d[0] = R::runif(0,1)* 2 *3.14159265358979323846; 
+  int hm=1;
+  for(int i=1; i<nsteps & t[i]<mm; i++){
+    hm=hm+1;
+    aux2=rvonmises(1,d[i-1],nk); 
+    t[i] = t[i-1] + vtm[i];
+    d[i] = aux2[0];
+    x[i] = x[i-1] + cos(d[i]) * (vls[i]) * vtm[i];
+    y[i] = y[i-1] + sin(d[i]) * (vls[i]) * vtm[i]; 
+    
+    
+  }
+  NumericMatrix sol(hm,3);
+  
+  sol(_,0)=x;
+  sol(_,1)=y;
+  sol(_,2)=t;
+ 
+  return(sol);
+  //return DataFrame::create(Named("tu")= tu);
+}
+
+
+NumericMatrix cppObsM(NumericVector x, NumericVector y, NumericVector t, double dt) {
+  
+  
+  NumericVector xs(x);
+  NumericVector ys(y);
+  NumericVector xt(t);
+  double dts = dt;
+  int nts = ceil(max(xt)/dts); //ceil(xt[n-1]/dts);
+  NumericVector st(nts);
+  NumericVector sx(nts);
+  NumericVector sy(nts);
+  NumericVector ite(nts); 
+  NumericMatrix sol(nts,3);
+  
+  
+  for(int i=1; i<nts; i++){
+    st[i] = st[i-1] + dts;
+    NumericVector::iterator it = std::upper_bound(xt.begin(), xt.end(), st[i]);
+    ite[i] = it -xt.begin()-1;
+    double dx = xs[ite[i]+1] - xs[ite[i]];
+    double dy = ys[ite[i]+1] - ys[ite[i]];
+    double dto = xt[ite[i]+1] - xt[ite[i]];
+    sx[i] = xs[ite[i]] + ((st[i]-xt[ite[i]])*dx)/dto;
+    sy[i] = ys[ite[i]] + ((st[i]-xt[ite[i]])*dy)/dto;
+  }
+  
+  sol(_,0)=sx;
+  sol(_,1)=sy;
+  sol(_,2)=st;
+  
+  
+  //NumericMatrix::create(Named("sx")= sx, Named("sy") = sy, Named("st") = st)
+  return sol;
+}
+
+
+// [[Rcpp::export]]
+List ABC_CRW(int nsims,int nsams,int maxt,int nobs,double ddt){
+  
+  int ntot=nsims;
+  int nobss=nobs;
+  int nsam=nsams;
+  int maxx=maxt;
+  double dt=ddt;
+  int sobs=1;
+  NumericMatrix sX(nobss, ntot);
+  NumericMatrix sY(nobss, ntot);
+  NumericMatrix sT(nobss, ntot);
+ 
+  // sample from priors
+    NumericVector sw(ntot);
+    NumericVector sk(ntot);
+  
+  sw = runif(nsims,0.1,10);
+  sk = runif(nsims,5,100);
+
+  //simulations
+  for(int i=0; i<ntot; i++)
+  {
+  // observed data
+  int dims=0;
+  //NumericMatrix tray(dims,3);
+  NumericMatrix tray(cppCRW_exp(sk[i],sw[i],nsam,maxx));
+    NumericMatrix Obss(dims,3);
+  Obss=cppObsM(tray(_,0),
+               tray(_,1),
+               tray(_,2),
+               ddt);
+  dims=Obss(_,0).size();
+  int sv=0;
+  sv=min(NumericVector::create(nobss,dims));
+  
+  //Rcout << sv << "\n";
+  if (sv<nobss){
+    NumericVector tail(nobss,NA_REAL);
+    sX(_,i)= tail;
+    sY(_,i)= tail;
+    sT(_,i)= tail;
+   
+  
+  }
+  
+  else{
+    //Rcout << Obss << "\n";
+    sX(_,i)= Obss(_,0);
+    sY(_,i)= Obss(_,1);
+    sT(_,i)= Obss(_,2);
+  }
+  
+  //Rcout << dims << "\n";
+  
+   }
+  return List::create(Named("sX")= sX,Named("sY")= sY,Named("sT")= sT,Named("w")=sw,
+                            Named("k")=sk);
+  }
+
--- SSM/cpp/simulacion_2.R
+++ SSM/cpp/simulacion_2.R
@@ -14,8 +14,8 @@ Rcpp::sourceCpp('/home/sofia/proyecto_doctoral/pruebas/SSM/cppObs.cpp')
 nsteps <- 800 # number of moves performed by the animal
 
 # movement parameters:
-t_w=3
-t_k = 20
+t_w=6
+t_k = 10
 dt <- 3 # time interval for observations
 
 # simulate true moves
@@ -50,7 +50,7 @@ a=ABC_CRW(nsims,1000,maxt,nobs,dt)
 ##################################### Summaries ########################################
 ########################################################################################
 
-Summ=matrix(NA,NN,9)
+Summ=matrix(NA,NN,10)
 ### for the real ones
 
 for (i in 1:NN)
@@ -65,10 +65,10 @@ for (i in 1:NN)
   aa=acf(ps$steps,plot=FALSE)
   #llmm=lm(aa$lag[2:length(aa$lag)]~aa$acf[2:length(aa$lag)])
   
- t2=(sum((oz$sx[1:(length(oz$sx)-1)]-oz$sx[2:(length(oz$sx))])^2))/(length(oz$sx)-1)/+
-   (sum((oz$sy[1:(length(oz$sy)-1)]-oz$sy[2:(length(oz$sy))])^2))/(length(oz$sy)-1)
-r2=sd(oz$sx)+sd(oz$sy)
-  
+   t2=(sum((oz$sx[1:(length(oz$sx)-1)]-oz$sx[2:(length(oz$sx))])^2))/(length(oz$sx)-1)/+
+     (sum((oz$sy[1:(length(oz$sy)-1)]-oz$sy[2:(length(oz$sy))])^2))/(length(oz$sy)-1)
+  r2=sd(oz$sx)+sd(oz$sy)
+    
   ct=mean(cos(ps$turns))
   st=mean(sin(ps$turns))
   bo=sd(ps$steps)#/abs(mean(ps$steps))
@@ -77,24 +77,24 @@ r2=sd(oz$sx)+sd(oz$sy)
            sd(ps$turns),
            cdt2(ps$steps,oz$st[2:length(oz$st)]),
            sd(ps$steps),
-           #mean(ps$turns),
-           #mean(aa$acf),
-           sqrt((mean(cos(ps$turns)))^2+(mean(sin(ps$turns)))^2),
-         #  cdt2(nsd(ps$steps),oz$st[2:length(oz$st)]))
-  it(ps$steps,oz$sx,oz$sy),
-  #sd.circular(circular(ps$direction)),
-  #angular.deviation(circular(ps$turns)),
-  si(ct,st,mean(ps$steps),bo),
-   t2,
-     r2)
+          #mean(ps$turns),
+          mean(aa$acf),
+          sqrt((mean(cos(ps$turns)))^2+(mean(sin(ps$turns)))^2),
+          #cdt2(nsd(ps$steps),oz$st[2:length(oz$st)]))
+          it(ps$steps,oz$sx,oz$sy),
+          #sd.circular(circular(ps$direction)),
+          #angular.deviation(circular(ps$turns)),
+          si(ct,st,mean(ps$steps),bo),
+          t2,
+          r2)
   
   
 }
 
-### for the simulation ones
+### for the simulated ones
 
 
-SSum=matrix(NA,nsims,9)
+SSum=matrix(NA,nsims,10)
 for (j in 1:nsims)
 {
   osx=a$sX[,j]
@@ -107,7 +107,7 @@ for (j in 1:nsims)
   #bb=acf(circular(ps$direction),plot=FALSE)
   #llmmm=lm(bb$lag[2:length(bb$lag)]~bb$acf[2:length(bb$lag)])
   
-  aa=acf(ps$steps,plot=FALSE)
+  aa=acf(na.omit(ps$steps),plot=FALSE)
   #llmm=lm(aa$lag[2:length(aa$lag)]~aa$acf[2:length(aa$lag)])
   
   t2=(sum((osx[1:(length(osx)-1)]-osx[2:(length(osx))])^2))/(length(osx)-1)/+
@@ -118,12 +118,12 @@ for (j in 1:nsims)
   st=mean(sin(ps$turns))
   bo=sd(ps$steps)#/abs(mean(ps$steps))
   
-  SSum[j,]<-c(mean(ps$steps),
+    SSum[j,]<-c(mean(ps$steps),
                sd(ps$turns),
                cdt2(ps$steps,ost[2:length(ost)]),
                sd(ps$steps),
                #mean(ps$turns),
-               #mean(aa$acf),
+               mean(aa$acf),
                sqrt((mean(cos(ps$turns)))^2+(mean(sin(ps$turns)))^2),
                #  cdt2(nsd(ps$steps),oz$st[2:length(oz$st)]))
                it(ps$steps,osx,osy),
@@ -135,11 +135,100 @@ for (j in 1:nsims)
  
 }
 
-View(SSum)
+#View(SSum)
 
 ########################################################################################
 ##################################### Compare ##########################################
 ########################################################################################
+
+
+#hist(Summ[,1])
+#hist(SSum[,1])
+
+nbest<-100
+quienes=c(1,5)
+
+st=Summ[,quienes]
+ss=SSum[,quienes]
+
+
+# Via distancia de Mahalanobis 
+Dists=matrix(NA,nsims,NN)
+
+Non=na.omit(SSum[,quienes])
+qq=which(is.finite((apply(Non,1,sum)))==T)
+
+indice=which(SSum[,1]==max(Non[qq,1]))
+
+
+var(na.omit(SSum[,1]))
+
+for (i in 1:NN)
+{
+  Dists[,i]=apply(ss,1,mahalanobis,center=st[i,],cov=A)
+}
+
+
+SumMahal=apply(Dists,1,sum)
+which=numeric(nsims)
+which[order(SumMahal)[1:nbest]]=1 
+
+#####################################################################################
+########################## Plot posterior and prior #################################
+#####################################################################################
+
+par(mfrow=c(1,3),mar=c(8,5,4,1))
+
+
+##### w
+density_scale=density(a$w[which==1],bw=0.4,from=-0.1,to=10)
+xscale <- seq(-0.5, 10.5, length=100)
+y_scale <- dunif(xscale,min = 0,max=10)
+
+plot(xscale,y_scale,type='l',main=expression(lambda),ylim=c(0,max(y_scale,max(density_scale$y)))
+     ,col='red',xlab = '',ylab = 'Density',cex.axis=1.5,
+     cex.main=2,cex.lab=1.5,lwd=2)
+
+abline(v=mean(a$w[which==1]),col='dodgerblue3',lwd=2)
+abline(v=t_w,col='darkorange3',lwd=2)
+lines(density_scale,lwd=2)
+
+
+##### k
+density_scale=density(a$k[which==1],bw=3,from=3,to=100)
+xscale <- seq(0,105, length=100)
+y_scale <- dunif(xscale,min = 3, max=100)
+
+plot(xscale,y_scale,type='l',main=expression(k),
+     ylim=c(0,max(y_scale,max(density_scale$y))),xlab='',col='red',ylab = '',
+     cex.main=2,cex.lab=1.5,lwd=2,cex.axis=1.5)
+abline(v=mean(a$k[which==1]),col='dodgerblue3',lwd=2)
+abline(v=t_k,col='darkorange3',lwd=2)
+lines(density_scale,lwd=2)
+
+##### joint 
+plot(a$w[which==1],a$k[which==1],xlim=c(-2,12),ylim=c(-5,105),pch=3,
+     xlab="w",ylab="k",main="Joint",cex.lab=1.5,cex.axis=1.5,cex.main=1.8)
+rect(0,100,10,4,border='red',lwd=2)
+points(mean(a$w[which==1]),mean(a$k[which==1]),col='dodgerblue3',pch=16,cex=1.5)
+points(t_w,t_k,col='darkorange3',pch=16,cex=1.5)
+
+
+
+
+
+
+#Legends
+legend(-115,0.0008,xpd=NA,bty='n',
+       horiz=TRUE,legend=c('Prior','True Value')
+       ,fill=c('red','darkorange3'),cex=2,xjust=0.5,yjust=2)
+
+
+
+legend(-115,-0.005,xpd=NA,bty='n',
+       horiz=TRUE,legend=c('Estimated Mean', 'Estimated Posterior'),
+       fill=c('dodgerblue3','black'),cex=2,xjust=0.5,yjust=2)
+
 
 
 
